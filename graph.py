33ad8e2775e5581a87f0a15e1664f445266cef65
Diego Pasqualin <diego.pasqualin@thesuite.com>
2024-06-28 11:45:21 -0300

    Initial commit

---
+++ graph.py
@@ -0,0 +1,141 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+from itertools import chain
+from typing import Any
+from contextlib import suppress
+
+
+@dataclass
+class Edge:
+    node_from: Node
+    node_to: Node
+
+    def __hash__(self) -> int:
+        return hash((self.node_from.nid, self.node_to.nid))
+
+    def __repr__(self) -> str:
+        return f"Edge({self.node_from} -> {self.node_to})"
+
+
+@dataclass
+class Node:
+    nid: int
+    label: str
+    title: str
+    attrs: dict[str, Any] = field(default_factory=dict)
+    metadata: dict[str, Any] = field(default_factory=dict)
+
+    edges_before: set[Edge] = field(default_factory=set)
+    edges_after: set[Edge] = field(default_factory=set)
+
+    def contribute_to_permissions(self) -> bool:
+        if self.has_attr("is_root", "True"):
+            return True
+        return False
+
+    def has_attr(self, key: str, value) -> bool:
+        attrs = {**self.attrs, "title": self.title, "label": self.label}
+        return key in self.attrs and attrs[key] == value
+
+    def __hash__(self) -> int:
+        return self.nid
+
+    def __repr__(self) -> str:
+        return f"Node({self.nid}, {self.label})"
+
+
+@dataclass
+class Graph:
+    nodes: set[Node] = field(default_factory=set)
+    edges: set[Edge] = field(default_factory=set)
+
+    def add_node(self, n: Node):
+        self.nodes.add(n)
+
+    def add_edge(self, e: Edge, symmetric: bool = False):
+        if e.node_from not in self:
+            self.add_node(e.node_from)
+        if e.node_to not in self:
+            self.add_node(e.node_to)
+
+        self.edges.add(e)
+        e.node_from.edges_after.add(e)
+        e.node_to.edges_before.add(e)
+        if symmetric:
+            e = Edge(e.node_to, e.node_from)
+            self.add_edge(e, symmetric=False)
+
+    def remove_node(self, n: Node):
+        if n not in self:
+            raise ValueError(f"Node {n} not found in graph")
+        for e in list(chain(n.edges_before, n.edges_after)):
+            self.remove_edge(e)
+        self.nodes.remove(n)
+
+    def remove_edge(self, e: Edge):
+        e.node_from.edges_after.remove(e)
+        e.node_to.edges_before.remove(e)
+        self.edges.remove(e)
+
+    def get_nodes_by_attr(self, key: str, value: Any) -> list[Node]:
+        return [n for n in self.nodes if n.has_attr(key, value)]
+
+    def get_node_by_label(self, label: str) -> Node:
+        for n in self.nodes:
+            if n.label == label:
+                return n
+        raise ValueError(f"Node with label {label} not found")
+
+    def get_node_by_id(self, nid: int) -> Node:
+        for n in self.nodes:
+            if n.nid == nid:
+                return n
+        raise ValueError(f"Node with id {nid} not found")
+
+    @staticmethod
+    def neighbors(node: Node) -> tuple[set[Node], set[Node]]:
+        nodes_before = {e.node_from for e in node.edges_before}
+        nodes_after = {e.node_to for e in node.edges_after}
+        return nodes_before, nodes_after
+
+    def prune(self) -> list[Node]:
+        removed = self.prune_intermediary_nodes()
+        removed += self.prune_isolated_nodes()
+        return removed
+
+    def prune_isolated_nodes(self) -> list[Node]:
+        nodes_removed = []
+        for n in set(self.nodes):
+            if n.contribute_to_permissions():
+                continue
+            if not n.edges_before and not n.edges_after:
+                print("removing isolated node", n)
+                self.remove_node(n)
+                nodes_removed.append(n)
+        return nodes_removed
+
+    def prune_intermediary_nodes(self):
+        nodes_removed = []
+        for n in set(self.nodes):
+            if n.contribute_to_permissions():
+                continue
+            nodes_before, nodes_after = self.neighbors(n)
+            if len(nodes_before) == 2 and nodes_before == nodes_after:
+                n1 = nodes_before.pop()
+                n2 = nodes_before.pop()
+                self.add_edge(Edge(n1, n2), symmetric=True)
+                self.remove_node(n)
+                nodes_removed.append(n)
+                print("removing node", n, "connecting", n1, "with", n2)
+
+        return nodes_removed
+
+    def __contains__(self, key: Node | Edge) -> bool:
+        if isinstance(key, Node):
+            return key in self.nodes
+        if isinstance(key, Edge):
+            return key in self.edges
+        return False
+
+
+
---
+++ hasura.py
@@ -0,0 +1,121 @@
+import json
+from typing import Any
+
+from pyvis.network import Network
+
+from graph import Graph, Node, Edge
+
+
+class HasuraParser:
+
+    @staticmethod
+    def read_metadata(filename: str) -> dict[str, Any]:
+        with open(filename) as f:
+            metadata = json.loads(f.read())
+        return metadata
+
+    def generate_graph(self, filename: str) -> Graph:
+
+        g = Graph()
+        metadata = self.read_metadata(filename)
+        tables = [i for i in metadata["sources"][0]["tables"]]
+
+        # Create nodes
+        for nid, t in enumerate(tables):
+            t_name = t["table"]["name"]
+            attrs = {
+                "role": self._get_table_role(t),
+                "is_root": str(self._is_root_table(t)),
+            }
+            node = Node(nid=nid, label=t_name, title=t_name, attrs=attrs)
+            g.add_node(node)
+
+        # Create edges
+        for t in tables:
+            t_name = t["table"]["name"]
+            src = g.get_node_by_label(t_name)
+
+            for r in t.get("array_relationships", []):
+                r_to = r["using"]["foreign_key_constraint_on"]["table"]["name"]
+                try:
+                    dst = g.get_node_by_label(r_to)
+                except ValueError:
+                    print(f"Ignoring invalid array relationship: {t_name} -> {r_to}")
+                    continue
+                edge = Edge(node_from=src, node_to=dst)
+                g.add_edge(edge)
+
+            for r in t.get("object_relationships", []):
+                r_to = r["name"]
+                try:
+                    dst = g.get_node_by_label(r_to)
+                except ValueError:
+                    print(f"Ignoring invalid array relationship: {t_name} -> {r_to}")
+                    continue
+                edge = Edge(node_from=src, node_to=dst)
+                g.add_edge(edge)
+
+        return g
+
+    @staticmethod
+    def generate_network(g: Graph) -> Network:
+        net = Network(
+            notebook=True,
+            cdn_resources="remote",
+            neighborhood_highlight=True,
+            select_menu=True,
+            directed=True,
+            layout=True,
+            filter_menu=True
+        )
+
+        # set the physics layout of the network
+        net.barnes_hut()
+
+        for n in g.nodes:
+            e_from, e_to = g.neighbors(n)
+            n_neighbours = len(e_from) + len(e_to)
+            n_size = 20 + min(n_neighbours, 20)
+            color = "red" if n.has_attr("is_root", "True") else "blue"
+            border = "yellow" if n.has_attr("role", "public_role") else "blue"
+            net.add_node(
+                n.nid,
+                label=n.label,
+                title=n.title,
+                size=n_size,
+                color=color,
+                **n.attrs,
+            )
+
+        for e in g.edges:
+            net.add_edge(
+                e.node_from.nid,
+                e.node_to.nid,
+                arrows={"from": True},
+                arrowStrikethrough=True,
+            )
+        net.toggle_physics(False)
+        net.show_buttons(filter_=["physics"])
+        return net
+
+    @staticmethod
+    def _get_table_role(table: dict[str, Any]) -> str | None:
+        for sp in table.get("select_permissions", []):
+            if role := sp.get("role"):
+                return role
+        return None
+
+    @staticmethod
+    def _is_root_table(table: dict[str, Any]) -> bool:
+        for sp in table.get("select_permissions", []):
+            qrf = sp["permission"].get("query_root_fields")
+            srf = sp["permission"].get("subscription_root_fields")
+
+            if qrf is None and srf is None:
+                return True
+            # TODO implement the case where the fields had ["select"]
+            if isinstance(qrf, list) and len(qrf) > 1:
+                return True
+            if isinstance(srf, list) and len(qrf) > 1:
+                return True
+        return False
